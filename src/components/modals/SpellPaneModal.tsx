27ac9d3025ea7259db995f5e2e93f5bc1b890512
Josh Lyon <joshlyon@udel.edu>
2021-12-14 17:01:32 -0500

    Spell modal works and fetches spells with cards and such

--- src/components/modals/SpellPaneModal.tsx
+++ src/components/modals/SpellPaneModal.tsx
@@ -1,51 +1,53 @@
 import React, { Dispatch, useEffect, useState } from 'react';
-import { Button, Modal, ModalBody, ModalFooter } from 'react-bootstrap';
+import { Button, ButtonGroup, Card, Col, Dropdown, DropdownButton, ListGroup, ListGroupItem, Modal, ModalBody, ModalFooter } from 'react-bootstrap';
 import ModalHeader from 'react-bootstrap/esm/ModalHeader';
 import { Spell } from '../../types/Spell';
 import { isSpellList, SpellList } from '../../types/SpellList';
-// import { SpellList } from '../types/SpellList';
+import '../../css/SpellPaneModal.css';
+
+enum SpellFilterGroup {
+    SCHOOL='School',
+    CLASS='Class'
+}
+
+const defaultSpellList: SpellList = {
+    count: 0,
+    next: null,
+    previous: null,
+    results: []
+};
 
 export function SpellPaneModal({show, setShow}: {show: boolean, setShow: Dispatch<React.SetStateAction<boolean>>}) {
-    const defaultSpellList: SpellList = {
-        count: 0,
-        next: null,
-        previous: null,
-        results: []
-    };
-    const [spellList, setSpellList] = useState<SpellList>(defaultSpellList);
+    const [spells, setSpells] = useState<Spell[]>([]);
+    const [uniqueClasses, setUniqueClasses] = useState<string[]>([]);
+    const [_uniqueSchools, setUniqueSchools] = useState<string[]>([]);
 
     useEffect(() => {
-        const fetchData = async () => {
-            let tempSpellList: SpellList = {...spellList};
-            let spells: Spell[] = [];
-            let pageNo = 1;
-            do {
-                const response = await fetch(`https://api.open5e.com/spells/?page=${pageNo}`, {
-                    method: 'GET',
-                    headers: {
-                        'content-type': 'application/json'
-                    }
-                });
-                const data = await response.json();
-                if (data && isSpellList(data)) {
-                    tempSpellList = data;
-                    spells = spells.concat(data.results);
-                }
-                pageNo++;
-                console.log(spells);
-            } while (tempSpellList.next !== null);
-            setSpellList({...spellList, results: spells});
-        };
-        fetchData();
+        const asyncWrapper = async () => setSpells(await fetchSpells(setUniqueClasses, setUniqueSchools));
+        asyncWrapper();
     }, []);
 
+    function classFilterHandler(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        e.preventDefault();
+        return;
+    }
+
     return (
-        <Modal show={show}>
-            <ModalHeader>Spell List</ModalHeader>
+        <Modal id='spell-modal' size='lg' backdrop='true' show={show}>
+            <ModalHeader>
+                Spell List
+                <Button variant='close' onClick={() => setShow(false)}/>
+            </ModalHeader>
             <ModalBody>
-                <div>
-                    Data is {spellList.results.map(spell => {
-                        return <div key={spell.name}>{spell.name}</div>;
+                <div className='filter-row'>
+                    <FilterDropdown
+                        title={SpellFilterGroup.CLASS}
+                        addFilterHandler={classFilterHandler}
+                        options={Array.from(uniqueClasses)}/>
+                </div>
+                <div className='spell-list'>
+                    {spells.map(spell => {
+                        return <SpellListing key={spell.name} spell={spell}/>;
                     })}
                 </div>
             </ModalBody>
@@ -56,3 +58,83 @@ export function SpellPaneModal({show, setShow}: {show: boolean, setShow: Dispatc
     );
 }
 
+function SpellListing({spell}: {spell: Spell}) {
+    return (
+        <Col className='spell-list-entry'>
+            <Card>
+                <Card.Body>
+                    <Card.Title>
+                        <div className='spell-header'>
+                            <div className='spell-header-main'>{spell.name}</div>
+                            <div className='spell-header-sub'>{spell.school}, {spell.level}</div>
+                        </div>
+                    </Card.Title>
+                    <div>{spell.desc}</div><br/><div>{spell.higher_level}</div>
+                    <ListGroup>
+                        <ListGroupItem>
+                            {spell.casting_time !== '' && <div className='spell-info'>Casting time: {spell.casting_time}</div>}
+                            {spell.dnd_class !== '' && <div className='spell-info'>Class(es): {spell.dnd_class}</div>}
+                            {spell.duration !== '' && <div className='spell-info'>Duration: {spell.duration}</div>}
+                            {spell.range !== '' && <div className='spell-info'>Range/Area: {spell.range}</div>}
+                            {spell.components !== '' && <div className='spell-info'>Components: {spell.components}</div>}
+                            {spell.material !== '' && <div className='spell-info'>Materials: {spell.material}</div>}
+                        </ListGroupItem>
+                    </ListGroup>
+                </Card.Body>
+                <Card.Footer>
+                    <ButtonGroup>
+                        <Button variant='success'>Add to spellbook</Button>
+                    </ButtonGroup>
+                </Card.Footer>
+            </Card>
+        </Col>
+    );
+}
+
+async function fetchSpells(
+    setUniqueClasses: React.Dispatch<React.SetStateAction<string[]>>,
+    setUniqueSchools: React.Dispatch<React.SetStateAction<string[]>>
+): Promise<Spell[]> {
+    let tempSpellList: SpellList = {...defaultSpellList};
+    let spells: Spell[] = [];
+    let pageNo = 1;
+    const uniqueClasses = new Set<string>();
+    const uniqueSchools = new Set<string>();
+
+    do {
+        const response = await fetch(`https://api.open5e.com/spells/?page=${pageNo++}`, {
+            method: 'GET',
+            headers: {
+                'content-type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        if (data && isSpellList(data)) {
+            tempSpellList = data;
+            spells = spells.concat(data.results);
+        }
+    } while (tempSpellList.next !== null);
+
+    spells.forEach(spell => {
+        spell.dnd_class.split(',').forEach(dnd_class => uniqueClasses.add(dnd_class.trim()));
+        uniqueSchools.add(spell.school);
+    });
+
+    setUniqueClasses(Array.from(uniqueClasses));
+    setUniqueSchools(Array.from(uniqueSchools));
+    return spells;
+}
+
+function FilterDropdown({options, title, addFilterHandler}: {
+    options: string[], title: string, addFilterHandler: React.MouseEventHandler<HTMLButtonElement>
+}) {
+    const [_activeFilters, _setActiveFilters] = useState<string[]>([]);
+
+    return (
+        <DropdownButton title={title}>
+            {options.map(option => {
+                return <Dropdown.Item onClick={addFilterHandler} key={option}>{option}</Dropdown.Item>;
+            })}
+        </DropdownButton>
+    );
+}
